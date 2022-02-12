52c05009b40bea20f4f837c41a7ed2a027d5829b
Raj <varasaniraj5@gmail.com>
2022-02-12 22:23:28 +0530

    Created auth routes: -login -signup -getuserdetails

--- server/routes.js
+++ server/routes.js
@@ -1,4 +1,5 @@
+import authRouter from "./api/controllers/auth/router";
 
 export default function routes(app) {
-  // app.use('/api/v1/examples', examplesRouter);
+  app.use("/api/v1/auth", authRouter);
 }
