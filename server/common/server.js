15f528f59f04280950577efb6f8f4a47a08ea569
Raj <varasaniraj5@gmail.com>
2022-02-12 21:54:36 +0530

    Reconfigured server

--- server/common/server.js
+++ server/common/server.js
@@ -1,52 +1,58 @@
-import middleware from 'swagger-express-middleware';
-import * as path from 'path';
-import errorHandler from '../api/middlewares/error.handler';
+import Express from "express";
 
-export default function swagger(app, routes) {
-  return new Promise((resolve, reject) => {
-    middleware(path.join(__dirname, 'api.yml'), app, (err, mw) => {
-      if (err) {
-        return reject(err);
-      }
-      // Enable Express' case-sensitive and strict options
-      // (so "/entities", "/Entities", and "/Entities/" are all different)
-      app.enable('case sensitive routing');
-      app.enable('strict routing');
+import cors from "cors";
 
-      app.use(mw.metadata());
-      app.use(mw.files({
-        // Override the Express App's case-sensitive 
-        // and strict-routing settings for the Files middleware.
-        caseSensitive: false,
-        strict: false,
-      }, {
-        useBasePath: false,
-        apiPath: process.env.SWAGGER_API_SPEC,
-        // Disable serving the "api.yml" file
-        // rawFilesPath: false
-      }));
+import * as path from "path";
+import * as bodyParser from "body-parser";
+import * as http from "http";
+import * as os from "os";
+import l from "./logger";
+import oas from "./swagger";
 
-      app.use(mw.parseRequest({
-        // Configure the cookie parser to use secure cookies
-        cookie: {
-          secret: process.env.SESSION_SECRET,
-        },
-        // Don't allow JSON content over 100kb (default is 1mb)
-        json: {
-          limit: process.env.REQUEST_LIMIT,
-        },
-      }));
+import mongo from "./mongo";
 
-      // These two middleware don't have any options (yet)
-      app.use(
-        mw.CORS(),
-        mw.validateRequest());
+const app = new Express();
 
-      routes(app);
+export default class ExpressServer {
+  constructor() {
+    const root = path.normalize(`${__dirname}/../..`);
 
-      // eslint-disable-next-line no-unused-vars, no-shadow
-      app.use(errorHandler);
-      return resolve();
-    });
-  });
+    app.use(bodyParser.json({ limit: process.env.REQUEST_LIMIT || "100kb" }));
+    app.use(
+      bodyParser.urlencoded({
+        extended: true,
+        limit: process.env.REQUEST_LIMIT || "100kb",
+      })
+    );
+    app.use(bodyParser.text({ limit: process.env.REQUEST_LIMIT || "100kb" }));
+
+    app.use(Express.static(`${root}/public`));
+
+    app.use(cors());
+  }
+
+  router(routes) {
+    this.routes = routes;
+    return this;
+  }
+
+  listen(port = process.env.PORT) {
+    const welcome = (p) => () =>
+      l.info(
+        `up and running in ${
+          process.env.NODE_ENV || "development"
+        } @: ${os.hostname()} on port: ${p}}`
+      );
+
+    oas(app, this.routes)
+      .then(() => {
+        l.info("Database Loaded!");
+        http.createServer(app).listen(port, welcome(port));
+      })
+      .catch((e) => {
+        l.error(e);
+      });
+
+    return app;
+  }
 }
