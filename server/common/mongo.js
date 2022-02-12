21d322a8c14d726978f69c31e52749765a05b181
Vansh Goyal <vgoyal1512@gmail.com>
2022-02-12 21:33:36 +0530

    Setup mongo

--- server/common/mongo.js
+++ server/common/mongo.js
@@ -1,12 +1,8 @@
-import mongoose from "mongoose";
-
-export default async () => {
-  const connection = await mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  });
-
-  return connection.connection.db;
-};
+const mongoose = require("mongoose");
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
--- server/common/server.js
+++ server/common/server.js
@@ -1,64 +1,52 @@
-import Express from 'express';
-
-
-import cors from 'cors';
-
+import middleware from 'swagger-express-middleware';
 import * as path from 'path';
-import * as bodyParser from 'body-parser';
-import * as http from 'http';
-import * as os from 'os';
-import l from './logger';
-import oas from './swagger';
+import errorHandler from '../api/middlewares/error.handler';
 
+export default function swagger(app, routes) {
+  return new Promise((resolve, reject) => {
+    middleware(path.join(__dirname, 'api.yml'), app, (err, mw) => {
+      if (err) {
+        return reject(err);
+      }
+      // Enable Express' case-sensitive and strict options
+      // (so "/entities", "/Entities", and "/Entities/" are all different)
+      app.enable('case sensitive routing');
+      app.enable('strict routing');
 
-import mongo from "./mongo";
+      app.use(mw.metadata());
+      app.use(mw.files({
+        // Override the Express App's case-sensitive 
+        // and strict-routing settings for the Files middleware.
+        caseSensitive: false,
+        strict: false,
+      }, {
+        useBasePath: false,
+        apiPath: process.env.SWAGGER_API_SPEC,
+        // Disable serving the "api.yml" file
+        // rawFilesPath: false
+      }));
 
+      app.use(mw.parseRequest({
+        // Configure the cookie parser to use secure cookies
+        cookie: {
+          secret: process.env.SESSION_SECRET,
+        },
+        // Don't allow JSON content over 100kb (default is 1mb)
+        json: {
+          limit: process.env.REQUEST_LIMIT,
+        },
+      }));
 
-const app = new Express();
+      // These two middleware don't have any options (yet)
+      app.use(
+        mw.CORS(),
+        mw.validateRequest());
 
-export default class ExpressServer {
-  constructor() {
-    const root = path.normalize(`${__dirname}/../..`);
+      routes(app);
 
-    app.use(bodyParser.json({ limit: process.env.REQUEST_LIMIT || '100kb' }));
-    app.use(bodyParser.urlencoded({ extended: true, limit: process.env.REQUEST_LIMIT || '100kb' }));
-    app.use(bodyParser.text({ limit: process.env.REQUEST_LIMIT || '100kb'}));
-
-    app.use(Express.static(`${root}/public`));
-
-
-    app.use(cors());
-
-  }
-
-  router(routes) {
-
-    this.routes = routes;
-    return this;
-
-  }
-
-  listen(port = process.env.PORT) {
-    const welcome = p => () =>
-      l.info(
-        `up and running in ${process.env.NODE_ENV ||
-          'development'} @: ${os.hostname()} on port: ${p}}`
-      );
-
-    oas(app, this.routes)
-      .then(() => {
-
-        mongo().then(() => {
-          l.info("Database Loaded!");
-          http.createServer(app).listen(port, welcome(port));
-        });
-
-      })
-      .catch((e) => {
-        l.error(e);
-        exit(1);
-      });
-
-    return app;
-  }
+      // eslint-disable-next-line no-unused-vars, no-shadow
+      app.use(errorHandler);
+      return resolve();
+    });
+  });
 }
